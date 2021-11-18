9e5648b4612bdd841a80deb31171d4cadce6ddad
Teerat <wongr003@umn.edu>
2021-11-18 08:24:43 -0600

    Started Aiyagari

--- Anmol/PS3/main.jl
+++ Anmol/PS3/main.jl
@@ -1,4 +1,17 @@
-using QuantEcon
+#### Aiyagari Model ####
 
-z_chain = MarkovChain([0.9 0.1; 0.1 0.9], [0.1; 1.0]);
-z_size = length(z_chain.state_values);
+## Packages 
+
+## Grid for A and E
+include("Tauchen.jl")
+
+a_min = 1e-10;
+a_max = 18.0;
+a_size = 5;
+a_vals = range(a_min, a_max, length = a_size);
+
+ρ = 0.6;
+σ = 0.3;
+μ = 0.0;
+n = 2;
+ϵ_vals, P_ϵ = Tauchen(μ, ρ, σ, n);
